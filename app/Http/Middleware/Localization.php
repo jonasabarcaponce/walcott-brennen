ebdc95cd1c2e3366643c2d018df2d6aad7a0cadd
Jonas Abarca <jonasabarca@iMac-de-Jonas.local>
2025-06-09 03:35:36 -0500

    Implements checking if user has a preference setted in database

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -16,16 +16,22 @@ class Localization
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Session::has('locale')) {
+        // Check if user is authenticated
+        $user = $request->user();
+        if ($user && isset($user->locale)) {
+            $locale = $user->locale;
+            if (array_key_exists($locale, config('app.available_locales'))) {
+                app()->setLocale($locale);
+            }
+        } elseif (Session::has('locale')) {
             $locale = Session::get('locale');
             if (array_key_exists($locale, config('app.available_locales'))) {
                 app()->setLocale($locale);
             }
         } else {
-            // Set default locale if not set in session
+            // Set default locale if not set in session or user
             app()->setLocale(config('app.fallback_locale'));
         }
         return $next($request); 
     }
 }
-
